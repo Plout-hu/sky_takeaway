4d2d4642be5c5fe5edd90d0162964c555317d808
Pluto-hu <571156946@qq.com>
2023-07-31 16:20:42 +0800

    公共字段自动填充功能开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.sky.service;
 
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import com.sky.result.PageResult;
 
 public interface EmployeeService {
@@ -14,7 +16,6 @@ public interface EmployeeService {
      * @return
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
-
     void save(EmployeeDTO employeeDTO);
 
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
@@ -22,6 +23,5 @@ public interface EmployeeService {
     void startOrStop(Integer status, Long id);
 
     Employee getById(Long id);
-
     void update(EmployeeDTO employeeDTO);
 }
